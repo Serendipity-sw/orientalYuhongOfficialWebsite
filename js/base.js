3e69f7c06a857d9bafa50a06647264ce1fc0188e
gloomysw <Sw435464>
2016-07-23 14:21:07 +0800

    项目初始化

---
+++ js/base.js
@@ -0,0 +1,6 @@
+/**
+ * Created by gloomysw on 2016/07/18.
+ */
+
+//遮罩层
+var mark=$('<div style="overflow: hidden; position: fixed; top: 0; left: 0; display: block; padding: 0; box-sizing: border-box; margin: 0; bottom: 0; background-color: black; z-index: 100; opacity: 0.5; right: 0;"></div>');
---
+++ js/formLayer.js
@@ -0,0 +1,3 @@
+/**
+ * Created by gloomysw on 2016/07/21.
+ */
